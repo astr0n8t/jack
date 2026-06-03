d0a0746a6b296c5de7f6583da16fd88ed6d5badf
Nathan Higley <13420786+astr0n8t@users.noreply.github.com>
2026-06-02 20:28:31 -0500

    Update ripper.py

--- jack/ripper.py
+++ jack/ripper.py
@@ -298,7 +298,7 @@ class JobRunner:
             self.send_webhook("error", finished)
         finally:
             if self.config.chown_user and self.config.chown_group:
-                chmod_cmd = ["chmod", "-R", self.config.chown_user + ":" + self.config.chown_group, str(output_dir)
+                chmod_cmd = ["chmod", "-R", self.config.chown_user + ":" + self.config.chown_group, str(output_dir)]
                 subprocess.Popen(chmod_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
             with self.lock:
                 self.starting.discard(device)
